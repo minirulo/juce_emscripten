e29258db294984ff934ffe438161f33e2c21f5f0
Kanru Hua <k.hua.kanru@ieee.org>
2019-12-25 17:19:52 +0900

    ComponentPeer: override browser ctrl + scroll.

--- modules/juce_gui_basics/native/juce_emscripten_Windowing.cpp
+++ modules/juce_gui_basics/native/juce_emscripten_Windowing.cpp
@@ -254,6 +254,10 @@ class EmscriptenComponentPeer : public ComponentPeer,
                 canvas.height = $4;
                 canvas.oncontextmenu = function(e) { e.preventDefault(); };
                 canvas.setAttribute('data-peer', $5);
+                canvas.addEventListener ('wheel', function(e) {
+                    if (event.ctrlKey)
+                        event.preventDefault();
+                }, true);
                 document.body.appendChild(canvas);
             }, id.toRawUTF8(), bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight(), this, ++highestZIndex);
             
